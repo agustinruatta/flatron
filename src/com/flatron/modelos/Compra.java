adc39cb9f127c765b73ecf198dc4f340139040cc
NicolasSantillan <nicoar_23@hotmail.com>
2018-09-15 11:33:21 -0300

    Actualizacion 1

--- src/com/flatron/modelos/Compra.java
+++ src/com/flatron/modelos/Compra.java
@@ -1,14 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 Nicolas Santillan <nicoar_23@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.flatron.modelos;
 
-/**
- *
- * @author laboratorio
- */
+import java.util.Date;
+
 public class Compra {
     
+    private Date fecha;
+    private int numeroComprobante;
+    private double totalCompra;
+
+    public Compra(Date fecha, int numeroComprobante, double totalCompra) {
+        this.fecha = fecha;
+        this.numeroComprobante = numeroComprobante;
+        this.totalCompra = totalCompra;
+    }
+
 }
--- src/com/flatron/modelos/DetalleCompra.java
+++ src/com/flatron/modelos/DetalleCompra.java
@@ -1,14 +1,35 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 Nicolas Santillan <nicoar_23@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.flatron.modelos;
 
-/**
- *
- * @author laboratorio
- */
 public class DetalleCompra {
     
+    private String nombreProveedor;
+    private String nombreProducto;
+    private String marcaProducto;
+    private String cantidadProducto;
+    private double totalProducto;
+
+    public DetalleCompra(String nombreProveedor, String nombreProducto, String marcaProducto, String cantidadProducto, double totalProducto) {
+        this.nombreProveedor = nombreProveedor;
+        this.nombreProducto = nombreProducto;
+        this.marcaProducto = marcaProducto;
+        this.cantidadProducto = cantidadProducto;
+        this.totalProducto = totalProducto;
+    }
+ 
 }
